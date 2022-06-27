37b150a866f815fa53386334e5b33eccb874940a
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-27 17:28:04 +0700

    fundamental

--- fundamental.py
+++ fundamental.py
@@ -1,3 +1,6 @@
 list = [1, 2, 3, 4, 5]
 
 print(list)
+
+# list merupakan sebuh collection type yang dapat menampung banyak tipe data didalamnya dengan fungsi hampir sama dengan array
+# akan tetapi dalam bahasa pemrograman python akan sering digunakannya sebuah list
