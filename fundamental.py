8c37917a99ad819c830f66296892824e7502623c
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-28 09:50:46 +0700

    list fundamental

--- fundamental.py
+++ fundamental.py
@@ -1,6 +1,15 @@
 list = [1, 2, 3, 4, 5]
+x = ["ferdian", "Firmansyah", True, 10]
 
+print(list)
+print(f"campur : {x}")
+
+# crud list
+# insert
+list.append(20)
 print(list)
 
 # list merupakan sebuh collection type yang dapat menampung banyak tipe data didalamnya dengan fungsi hampir sama dengan array
 # akan tetapi dalam bahasa pemrograman python akan sering digunakannya sebuah list
+# yang digunakan untuk menampung sekumpulan nilai yang akan ditempatkan didalam penanda kurung siku  []
+# perbedaan antara array dan list adalah array hanya dapat menampung sebuah tipe data yang harus sama sedangkan list dapat menampung banyak tipe data sehingga memudahkan ketika ingin menghasilkan sebuah tipe data baru
