4b17a07564b0213724dba035b6b817734e216920
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-28 09:57:49 +0700

    crud list

---
+++ list.py
@@ -0,0 +1,21 @@
+a = [10, 20, 30, 60, 50, 40, 90, 80, 70]
+
+print(a)
+
+# insert list
+a.append(100)
+print(a)
+
+a.insert(2, 140)
+print(a)
+
+# delete
+a.pop(2)
+print(a)
+
+# remove
+# a.remove(20)
+
+# update
+a[2] = 120
+print(a)
